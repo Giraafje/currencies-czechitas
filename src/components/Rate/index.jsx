c183e8c36731408e31817bd2caa14d61107d77cf
Giraafje <lenka.erbenova79@gmail.com>
2023-03-16 19:37:11 +0100

    Currencies are fetched and working.

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
 
-const currencies = {
-  USD: { CZK: 23.823 },
-  EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
-};
+// const currencies = {
+//   USD: { CZK: 23.823 },
+//   EUR: { CZK: 24.74 },
+//   GBP: { CZK: 29.067 }
+// };
 
-const Rate = () => {
+const Rate = ({from}) => {
+  const [rate, setRate] = useState(0.00)
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+    .then((response) => response.json())
+    .then((data) => setRate(data.rates.CZK));
+  }, [from])
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      {/* <div className="rate__value">{currencies[from].CZK} CZK</div> */}
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
